0383414ad0056a60559db6e4b68aa41e503c199b
gaimerI17 <gaimeri255@gmail.com>
2025-05-06 21:58:56 +0300

    Update success.c - typo part 3

--- many-lines/success.c
+++ many-lines/success.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!"\n);
-  printf("I'm trying to learn C."\n);
-  printf("I seem to like it."\n);
+  printf("Hello, World!\n");
+  printf("I'm trying to learn C.\n");
+  printf("I seem to like it.\n");
 }
