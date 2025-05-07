a689a9be39c252429d649fbaadfb0a28bce02ee4
gaimerI17 <gaimeri255@gmail.com>
2025-05-07 15:11:20 +0300

    Create types.c

---
+++ types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int a = 15;
+  float b = 20.99;
+  double c = 63.7953;
+  char d = 'a';
+  // Strings are arrays of chars
+  
+  printf("Integer: %d\n", a);
+  printf("Float: %f\n", b);
+  printf("Double: %lf\n", c);
+  printf("Char: %c\n", d);
+}
