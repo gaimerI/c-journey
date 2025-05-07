beb8c08abcb617825a504821354fc027ed67872c
gaimerI17 <gaimeri255@gmail.com>
2025-05-07 15:07:22 +0300

    Create math.c

---
+++ math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int a = 15;
+  int b = 20;
+
+  int sum = a + b;
+  int difference = a - b;
+  int product = a * b;
+  float ratio = a / b;
+
+  printf("First number: %d\n", a);
+  printf("Second number: %d\n", b);
+  printf("Sum: %d\n", sum);
+  printf("Difference: %d\n", difference);
+  printf("Product: %d\n", product);
+  printf("Ratio: %f\n", ratio);
+}
