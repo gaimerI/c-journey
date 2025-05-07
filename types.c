bd48d0db5d52b5512c5c2254d931675b0f01e940
gaimerI17 <gaimeri255@gmail.com>
2025-05-07 17:28:39 +0300

    Update types.c

--- types.c
+++ types.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
   int a = 15;
@@ -6,9 +7,11 @@ int main() {
   double c = 63.7953;
   char d = 'a'; // Chars can also use numeric ASCII values, like 65 for 'a'
   // Strings are arrays of chars
+  bool f = true; // Booleans are not built in, they require stdbool.h
   
   printf("Integer: %d\n", a);
   printf("Float: %f\n", b);
   printf("Double: %lf\n", c);
   printf("Char: %c\n", d);
+  printf("Bool: %d\n", f); // Booleans are processed as 0 or 1
 }
