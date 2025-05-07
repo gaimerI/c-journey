8bc9be6843e4ad450fb0429bf63b1eec91426838
gaimerI17 <gaimeri255@gmail.com>
2025-05-07 07:39:19 +0300

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!"); // Short comment, JS-like
+  /* 
+    Alright.
+    This is a long, long comment.
+    It spans multiple lines.
+    Originally you HAD to use these.
+  */
+}
