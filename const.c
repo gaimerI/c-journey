aae9b47b85b1c12efc436aa1f4e7129c3086eb0e
gaimerI17 <gaimeri255@gmail.com>
2025-05-07 17:25:50 +0300

    Create const.c

---
+++ const.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  const int A = 15; // It's good practise to write constant names uppercase.
+  const float B = 20.99;
+  const double C = 63.7953;
+  const char D = 'a';
+  
+  printf("Integer: %lu\n", sizeof(a));
+  printf("Float: %lu\n", sizeof(b));
+  printf("Double: %lu\n", sizeof(c));
+  printf("Char: %lu\n", sizeof(d));
+}
