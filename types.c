201a8e1c6de284bedd00deb632e95aae8afab8f4
gaimerI17 <gaimeri255@gmail.com>
2025-05-07 18:28:04 +0300

    Update types.c

--- types.c
+++ types.c
@@ -8,10 +8,13 @@ int main() {
   char d = 'a'; // Chars can also use numeric ASCII values, like 65 for 'a'
   // Strings are arrays of chars
   bool f = true; // Booleans are not built in, they require stdbool.h
+  int g[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Array, all of same type
   
   printf("Integer: %d\n", a);
   printf("Float: %f\n", b);
   printf("Double: %lf\n", c);
   printf("Char: %c\n", d);
   printf("Bool: %d\n", f); // Booleans are processed as 0 or 1
+  printf("Array: %d\n", g); // This actually fails, due to the array having many ints
+  printf("Array item: %d\n", g[0]); // Array items are called with an index
 }
